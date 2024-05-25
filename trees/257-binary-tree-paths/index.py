b0b4ab47b4421d1a08fcb6620965eea8cc0b8d72
illia goncharov <goncharovphp@gmail.com>
2024-05-24 17:32:28 -0700

    257. Binary Tree Paths

---
+++ trees/257-binary-tree-paths/index.py
@@ -0,0 +1,13 @@
+class Solution:
+    def binaryTreePaths(self, root):
+        res = []
+        def dfs(node, path = ""):
+            if not node:
+                return
+            path += str(node.val)
+            if not node.left and not node.right:
+                res.append(path)
+            dfs(node.left, path + "->")
+            dfs(node.right, path + "->")
+        dfs(root, "")
+        return res
